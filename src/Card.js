a7f6baa068912b4d4353c7ebb41bf673a881b52f
Yassin Elsisy <136658336+YassinElsisy@users.noreply.github.com>
2023-08-24 17:36:43 -0400

    Add files via upload

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="">
+      <img src="" className="" alt="robots"></img>
+      <div className="card-body">
+        <h2 className="">Card title</h2>
+        <p className="">
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
